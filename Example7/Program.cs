b153f7da115023e2498f25a0f4e08b20bb2a91b1
syedkhaleel2010 <syedkhaleel2010@gmail.com>
2017-08-01 20:11:34 +0400

    dfdsd

--- Example7/Program.cs
+++ Example7/Program.cs
@@ -15,9 +15,9 @@ namespace Example7
             int d, r = 0;
             while (n > 0)
             {
-                d = n % 10; // 123  =>3  ,=>2  => 1
-                r = r * 10 + d; // 3  ==> 3 * 10 + 2 = 32 => 32 * 10 + 1 = 321
-                n = n / 10;// 12   => 1  ==>0
+                d = n % 10; 
+                r = r * 10 + d; 
+                n = n / 10;
 
             }
             Console.WriteLine("Reverse of a given number is {0}", r);
